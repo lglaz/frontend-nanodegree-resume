eeb698d0c366dcde5c661a0cb3fa7e2d7b68349b
Lukasz Glaz <l.glaz@samsung.com>
2014-10-28 16:03:46 +0100

    first lesson finished

---
+++ js/resumeBuilder.js
@@ -0,0 +1,57 @@
+var bio = {
+    "name": "Lukasz Glaz",
+    "role": "developer",
+    "contacts": "info",
+    "pictureURL": "images/fry.jpg",
+    "skills": [
+        "programming",
+        "design"
+    ]
+};
+
+
+// $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+// $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+// $("#header").prepend(HTMLbioPic.replace("%data%", bio.pictureURL));
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Samsung",
+            "title": "Software Engineer",
+            "location": "Poznan",
+            "dates": "2014-now",
+            "description": ""
+        },
+        {
+            "employer": "PSI",
+            "title": "Software Engineer",
+            "location": "Poznan",
+            "dates": "2010-2014",
+            "description": ""
+        },
+        {
+            "employer": "Dantek sp. z o.o.",
+            "title": "Software Engineer",
+            "location": "Poznan",
+            "dates": "2003-2010",
+            "description": ""
+        }
+    ]
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "Szczecin Univercity of Technology",
+            "degree": "Msc",
+            "majors": [
+                "Computer Science"
+            ],
+            "location": "Szczecin",
+            "dates": "1997-2003",
+            "url": ""
+        }
+    ],
+    "onlineCourses": []
+};
